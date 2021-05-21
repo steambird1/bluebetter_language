e6297c8bde6117bb1c69a505330175ac776a5ec7
steambird1 <steambird@163.com>
2021-05-22 06:21:41 +0800

    build v4.1

--- b3int_private.h
+++ b3int_private.h
@@ -5,33 +5,19 @@
 #define B3INT_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-<<<<<<< HEAD
-#define VER_STRING	"4.1.0.208"
-=======
-#define VER_STRING	"4.0.0.179"
->>>>>>> main
+#define VER_STRING	"4.1.0.180"
 #define VER_MAJOR	4
 #define VER_MINOR	1
 #define VER_RELEASE	0
-<<<<<<< HEAD
-#define VER_BUILD	208
+#define VER_BUILD	180
 #define COMPANY_NAME	""
-#define FILE_VERSION	"4.1.0.208"
-=======
-#define VER_BUILD	179
-#define COMPANY_NAME	""
-#define FILE_VERSION	"4.0.0.179"
->>>>>>> main
+#define FILE_VERSION	"4.1.0.180"
 #define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
 #define INTERNAL_NAME	""
 #define LEGAL_COPYRIGHT	""
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
 #define PRODUCT_NAME	"BlueBetter Interpreter"
-<<<<<<< HEAD
-#define PRODUCT_VERSION	"4.1.0.208"
-=======
-#define PRODUCT_VERSION	"4.0.0.179"
->>>>>>> main
+#define PRODUCT_VERSION	"4.1.0.180"
 
 #endif /*B3INT_PRIVATE_H*/
